1025fb028e72fed282c17eab5418ebbb80f41196
silvershades <capriulomatias@gmail.com>
2021-09-14 17:43:07 -0300

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@100;200;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
 <div class="container mx-auto ">
@@ -20,8 +20,9 @@
         <div class=""></div>
         <div class="col-span-4 align-middle text-center">
             <img src="{{asset("img/logo.svg")}}" class="object-contain h-48 mx-auto" alt="black sun labs logo">
-            <h1 class="text-black-50 text-5xl font-bold mx-auto mt-10">BLACK SUN LABS</h1>
-            <p class="font-thin text-xl">WEB DEVELOPMENT</p>
+            <h1 class="text-black-50 text-5xl font-bold mx-auto mt-10">BSL</h1>
+            <h1 class="text-black-50 text-5xl font-medium mx-auto mt-10">Black Sun Labs</h1>
+            <p class="font-light text-xl">WEB DEVELOPMENT</p>
         </div>
         <div class=""></div>
     </div>

cfb98c2fed46429d5060c5cad80e63bd72d49bb3
silvershades <capriulomatias@gmail.com>
2021-09-15 17:59:14 -0300

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,19 +14,48 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@100;200;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap" rel="stylesheet">
 </head>
-<body>
-<div class="container mx-auto ">
-    <div class="grid grid-cols-6 mt-40">
-        <div class=""></div>
-        <div class="col-span-4 align-middle text-center">
-            <img src="{{asset("img/logo.svg")}}" class="object-contain h-48 mx-auto" alt="black sun labs logo">
-            <h1 class="text-black-50 text-5xl font-medium mx-auto mt-10">Black Sun Labs</h1>
-            <p class="font-light text-xl">WEB DEVELOPMENT</p>
+<body class="font-serif text-gray-900">
+<header class="h-20" id="navbar">
+    <nav class="container mx-auto h-full">
+        <ul class="grid grid-cols-5 gap-4 text-center items-center h-20 text-lg">
+            <li class=""><a href="#">About us</a></li>
+            <li class=""><a href="#">Services</a></li>
+            <li class=""><a href="#">Portfolio</a></li>
+            <li class=""><a href="#">Contact</a></li>
+            <li class=""><a href="#">Clients</a></li>
+
+        </ul>
+    </nav>
+</header>
+<main class="container mx-auto">
+    <div class="flex items-center h-40 space-x-10 p-20 mt-8">
+        <img src="{{asset("img/logo.svg")}}" alt="Black Sun Labs logo" class="w-36 inline-block">
+        <div>
+            <h1 class="text-6xl font-bold inline-block text-right mb-2">Black Sun <span class="font-thin">Labs</span></h1>
+            <p class="font-mono text-lg">{Web and software development;}</p>
         </div>
-        <div class=""></div>
     </div>
-</div>
+    <div class="mt-20">
+
+        <p class="text-7xl text-right inline-block">
+            <span class="text-7xl text-right inline-block font-light text-gray-500">array[1] = </span>
+            <span class="text-yellow-500 "> ["</span>
+            Let's work together and create something unique for your needs!
+            <span class="text-yellow-500 "> "]</span>
+        </p>
+        <p class="font-mono text-lg text-right mt-3">{Creating solutions since 2006}</p>
+    </div>
+    <div class="grid grid-cols-2 mt-20 text-center">
+        <div><a href="#" class=" p-3 rounded-sm text-2xl"><span class="text-gray-500 font-mono">execute</span> Services<span class="text-gray-500">()</span></a></div>
+        <div><a href="#" class=" p-3 rounded-sm text-2xl"><span class="text-gray-500 font-mono">execute</span> Contact_Us<span class="text-gray-500">()</span></a></div>
+
+    </div>
+
+</main>
+
 
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            mono: ['Fira Mono', 'monospace'],
+            serif: ['Urbanist', 'serif'],
+        },
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }

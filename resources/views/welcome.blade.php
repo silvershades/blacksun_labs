b1dc663da3625ad2863b1713e5936bc3a2403ff4
silvershades <capriulomatias@gmail.com>
2021-09-16 16:00:35 -0300

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,31 +31,76 @@
     </nav>
 </header>
 <main class="container mx-auto">
-    <div class="flex items-center h-40 space-x-10 p-20 mt-8">
-        <img src="{{asset("img/logo.svg")}}" alt="Black Sun Labs logo" class="w-36 inline-block">
-        <div>
-            <h1 class="text-6xl font-bold inline-block text-right mb-2">Black Sun <span class="font-thin">Labs</span></h1>
-            <p class="font-mono text-lg">{Web and software development;}</p>
+    <section class="h-screen" id="home_page">
+        <div class="flex items-center h-40 space-x-10 p-20 mt-8">
+            <img src="{{asset("img/logo.svg")}}" alt="Black Sun Labs logo" class="w-28 inline-block">
+            <div>
+                <h1 class="text-6xl font-bold inline-block text-right mb-2">Black Sun <span class="font-thin">Labs</span></h1>
+                <p class="font-mono text-lg">{Web and software development}</p>
+            </div>
         </div>
-    </div>
-    <div class="mt-20">
+        <div class="mt-20 ">
+            <p class="text-7xl text-right inline-block">
+                <span class="text-7xl text-right font-light text-gray-300">array[1] = </span>
+                <span class="text-yellow-400 "> ["</span>
+                Let's work together and create something unique for your needs!
+                <span class="text-yellow-400 "> "]</span>
+            </p>
+            <p class="font-mono text-lg text-right mt-3">{Creating solutions since 2006}</p>
+        </div>
+        <div class=" mt-20 text-center space-x-10">
+            <a href="#" class="bg-yellow-400 p-4 text-3xl rounded">See more</a>
+            <a href="#" class="bg-yellow-400 p-4 text-3xl rounded">Write to us</a>
+        </div>
+    </section>
+    <section class="h-screen" id="services">
+        <h2 class="text-7xl mb-16"><span class=" text-gray-200 ">$</span> Services <span class="cursor_fx font-extrabold text-yellow-400">|</span></h2>
+        <div class="grid grid-cols-3 gap-4 items-center">
+            <div>
+                <img src="{{asset("img/web_design.svg")}}" alt="web design svg">
+            </div>
+            <div class="col-span-2 px-5">
+                <h3 class="text-6xl">Web<span class="font-light text-gray-300">_</span>development<span class="font-light text-gray-300">();</span></h3>
 
-        <p class="text-7xl text-right inline-block">
-            <span class="text-7xl text-right inline-block font-light text-gray-500">array[1] = </span>
-            <span class="text-yellow-500 "> ["</span>
-            Let's work together and create something unique for your needs!
-            <span class="text-yellow-500 "> "]</span>
-        </p>
-        <p class="font-mono text-lg text-right mt-3">{Creating solutions since 2006}</p>
-    </div>
-    <div class="grid grid-cols-2 mt-20 text-center">
-        <div><a href="#" class=" p-3 rounded-sm text-2xl"><span class="text-gray-500 font-mono">execute</span> Services<span class="text-gray-500">()</span></a></div>
-        <div><a href="#" class=" p-3 rounded-sm text-2xl"><span class="text-gray-500 font-mono">execute</span> Contact_Us<span class="text-gray-500">()</span></a></div>
+                <h4 class="mt-5 text-yellow-400  text-3xl">FRONTEND DESIGN</h4>
+                <h5 class="font-mono text-lg">{Responsive - Innovative - SEO - Compatible}</h5>
+                <p class="mt-1 text-xl  ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, autem consequatur cupiditate delectus deserunt dolor ea eaque est, impedit ipsum
+                    itaque
+                    maxime nam numquam quisquam sed velit voluptatibus. Fugit, optio. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet, assumenda aut consequatur
+                    consequuntur, eveniet fugiat in incidunt magni, </p>
+                <h4 class="mt-5 text-yellow-400  text-3xl">BACKEND DEVELOPMENT</h4>
+                <h5 class="font-mono text-lg">{Robust - Secure - No Shorcuts - Escalable}</h5>
+                <p class="mt-1 text-xl  ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, autem consequatur cupiditate delectus deserunt dolor ea eaque est, impedit ipsum
+                    itaque
+                    maxime nam numquam quisquam sed velit voluptatibus. Fugit, optio. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet, assumenda aut consequatur
+                    consequuntur, eveniet fugiat in incidunt magni, </p>
+                <h4 class="mt-5 text-yellow-400  text-3xl">BRAND CREATION</h4>
+                <h5 class="font-mono text-lg">{Respect - Adapt - Represent - Success}</h5>
+                <p class="mt-1 text-xl  ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, autem consequatur cupiditate delectus deserunt dolor ea eaque est, impedit ipsum
+                    itaque
+                    maxime nam numquam quisquam sed velit voluptatibus. Fugit, optio. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet, assumenda aut consequatur
+                    consequuntur, eveniet fugiat in incidunt magni, </p>
 
-    </div>
+
+            </div>
+        </div>
+
+
+    </section>
 
 </main>
 
 
 </body>
+<script>
+    var cursors = document.getElementsByClassName("cursor_fx");
+    console.log(cursors);
+    for (cursor in cursors) {
+        if (cursor.style.display === "none") {
+            cursor.style.display = "block";
+        } else {
+            cursor.style.display = "none";
+        }
+    }
+</script>
 </html>

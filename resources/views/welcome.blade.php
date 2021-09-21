1889726a0fe75e997ac94896715dd96ca4a5eb57
silvershades <capriulomatias@gmail.com>
2021-09-21 01:04:30 -0300

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
 
                 <p class="font-mono text-lg text-gray-500">  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-                    </svg> Welcome to BSL! Let's work togheter to create something unique for your needs.</p>
+                    </svg> Welcome to BSL! Let's work together to create something unique for your needs.</p>
 {{--                <a href="#" class="border-blue-400 border-2 p-4 rounded text-2xl mt-20">Write to us</a>--}}
             </div>
 
